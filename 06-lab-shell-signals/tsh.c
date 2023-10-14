f875100102dad885b7a90d6a9ace8dcd419741d8
colerusson <colerusson2@gmail.com>
2023-10-14 11:51:55 -0600

    updates to tsh

--- 06-lab-shell-signals/tsh.c
+++ 06-lab-shell-signals/tsh.c
@@ -378,7 +378,12 @@ void do_bgfg(char **argv)
     if (job == NULL) {
         // Print an error if the job doesn't exist
         if (isJobID) {
-            printf("%s: No such job\n", arg);
+            // check if the number is valid and if not print no such process
+            if (id == 0 || id > maxjid(jobs)) {
+                printf("%s: No such process\n", arg);
+            } else {
+                printf("%s: No such job\n", arg);
+            }
         } else {
             if (strcmp(cmd, "fg") == 0) {
                 printf("fg: argument must be a PID or %%jobid\n");
