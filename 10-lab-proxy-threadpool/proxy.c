562057d4b673aa624f104d5cbec9a1a6440da287
colerusson <colerusson2@gmail.com>
2023-11-16 17:54:38 -0700

    work on proxy.c

--- 10-lab-proxy-threadpool/proxy.c
+++ 10-lab-proxy-threadpool/proxy.c
@@ -64,8 +64,10 @@ int complete_request_received(char *request, ssize_t received_bytes) {
     if (received_bytes > 0) {
         char *end_of_headers = strstr(request, "\r\n\r\n");
         if (end_of_headers != NULL && (end_of_headers - request) < received_bytes - 4) {
+            printf("Printing end of headers complete: %s\n", end_of_headers);
             return 1; // Request is complete (proper end of headers found)
         }
+        printf("Printing end of headers incomplete: %s\n", end_of_headers);
     }
     return 0; // Request is not complete
 }
