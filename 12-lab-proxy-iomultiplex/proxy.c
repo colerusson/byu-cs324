b46e29a73ffee8beb3bbcc69861c13ba0f4fe377
colerusson <colerusson2@gmail.com>
2023-12-08 18:36:41 -0700

    work on proxy.c

--- 12-lab-proxy-iomultiplex/proxy.c
+++ 12-lab-proxy-iomultiplex/proxy.c
@@ -6,25 +6,36 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <sys/select.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <ctype.h>
 
 /* Recommended max object size */
 #define MAX_BUFFER_SIZE 1024
 #define MAX_CLIENTS 100
 
-// Define a structure to hold socket descriptors in the buffer
-typedef struct {
-    int client_fd;
-} BufferItem;
-
-BufferItem buffer[MAX_BUFFER_SIZE];
-int in = 0, out = 0;
+#define READ_REQUEST 0
+#define SEND_REQUEST 1
 
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0";
 
+struct request_info {
+    int client_fd;           // client-to-proxy socket
+    int server_fd;           // proxy-to-server socket
+    int state;               // current state of the request (READ_REQUEST, SEND_REQUEST, etc.)
+    char buffer[MAX_BUFFER_SIZE];  // buffer to read into and write from
+    // other members to track bytes read/written, method, hostname, port, path, etc.
+};
+
 int parse_request(char *, char *, char *, char *, char *);
 int open_sfd(int);
-void handle_client(int);
+void handle_client(int, int, struct request_info *);
+void handle_new_clients(int, int, struct epoll_event *, struct request_info *);
 
+// TODO: Modify this function as needed
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
@@ -32,86 +43,138 @@ int main(int argc, char *argv[]) {
     }
 
     int port = atoi(argv[1]);
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        perror("Socket creation failed");
+    int server_fd = open_sfd(port);
+
+    int epoll_fd = epoll_create(1);
+    if (epoll_fd == -1) {
+        perror("Epoll creation failed");
         exit(EXIT_FAILURE);
     }
 
-    int optval = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) == -1) {
-        perror("Setsockopt failed");
+    struct epoll_event event;
+    event.events = EPOLLIN; // Define the event type as EPOLLIN
+    event.data.fd = server_fd; // Assuming server_fd has been defined earlier
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+        perror("Epoll add error");
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-        perror("Bind failed");
+    struct epoll_event *events = malloc(MAX_CLIENTS * sizeof(struct epoll_event));
+    if (events == NULL) {
+        perror("Events malloc error");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, MAX_CLIENTS) == -1) {
-        perror("Listen failed");
+    struct request_info *requests = calloc(MAX_CLIENTS, sizeof(struct request_info));
+    if (requests == NULL) {
+        perror("Requests calloc error");
         exit(EXIT_FAILURE);
     }
-
-    fd_set read_fds;
-    int max_fd = server_fd;
-    int client_fds[MAX_CLIENTS] = {0};
 
     while (1) {
-        FD_ZERO(&read_fds);
-        FD_SET(server_fd, &read_fds);
-
-        for (int i = 0; i < MAX_CLIENTS; ++i) {
-            if (client_fds[i] > 0) {
-                FD_SET(client_fds[i], &read_fds);
-                if (client_fds[i] > max_fd) {
-                    max_fd = client_fds[i];
-                }
-            }
-        }
-
-        int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
-        if (activity < 0) {
-            perror("Select error");
+        int num_ready = epoll_wait(epoll_fd, events, MAX_CLIENTS, -1);
+        if (num_ready == -1) {
+            perror("Epoll wait error");
             exit(EXIT_FAILURE);
         }
 
-        if (FD_ISSET(server_fd, &read_fds)) {
-            struct sockaddr_in client_addr;
-            socklen_t client_len = sizeof(client_addr);
-
-            int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-            if (client_fd == -1) {
-                perror("Accept failed");
-                continue;
-            }
-
-            for (int i = 0; i < MAX_CLIENTS; ++i) {
-                if (client_fds[i] == 0) {
-                    client_fds[i] = client_fd;
-                    break;
-                }
-            }
-        }
-
-        for (int i = 0; i < MAX_CLIENTS; ++i) {
-            if (client_fds[i] > 0 && FD_ISSET(client_fds[i], &read_fds)) {
-                handle_client(client_fds[i]);
-                close(client_fds[i]);
-                client_fds[i] = 0;
+        for (int i = 0; i < num_ready; ++i) {
+            if (events[i].data.fd == server_fd) {
+                handle_new_clients(epoll_fd, server_fd, events, requests);
+            } else {
+                handle_client(epoll_fd, events[i].data.fd, requests);
             }
         }
     }
 
+    free(events);
+    free(requests);
     close(server_fd);
+    close(epoll_fd);
     return 0;
+}
+
+// TODO: Modify this function as needed
+void handle_client(int epoll_fd, int client_fd, struct request_info *requests) {
+    int i;
+    for (i = 0; i < MAX_CLIENTS; ++i) {
+        if (requests[i].client_fd == client_fd) {
+            break;
+        }
+    }
+
+    if (i == MAX_CLIENTS) {
+        fprintf(stderr, "Client not found\n");
+        return;
+    }
+
+    int server_fd = open_sfd(epoll_fd);
+    if (server_fd == -1) {
+        fprintf(stderr, "Failed to connect to server\n");
+        return;
+    }
+
+    requests[i].server_fd = server_fd;
+    requests[i].state = READ_REQUEST;
+
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+        perror("Epoll add error");
+        close(server_fd);
+        return;
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = client_fd;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event) == -1) {
+        perror("Epoll add error");
+        close(client_fd);
+        return;
+    }
+
+    printf("Connected to server\n");
+}
+
+
+// TODO: Modify this function as needed
+void handle_new_clients(int epoll_fd, int server_fd, struct epoll_event *events, struct request_info *requests) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+    if (client_fd == -1) {
+        perror("Accept failed");
+        return;
+    }
+
+    int i;
+    for (i = 0; i < MAX_CLIENTS; ++i) {
+        if (requests[i].client_fd == 0) {
+            requests[i].client_fd = client_fd;
+            break;
+        }
+    }
+
+    if (i == MAX_CLIENTS) {
+        fprintf(stderr, "Too many clients\n");
+        close(client_fd);
+        return;
+    }
+
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = client_fd;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event) == -1) {
+        perror("Epoll add error");
+        close(client_fd);
+        return;
+    }
 }
 
 int parse_request(char *request, char *method, char *hostname, char *port, char *path) {
@@ -173,104 +236,6 @@ int parse_request(char *request, char *method, char *hostname, char *port, char 
         printf("URL not found\n");
         return 0; // URL not found
     }
-}
-
-void handle_client(int client_fd) {
-    char buffer[MAX_BUFFER_SIZE];
-    char method[16], hostname[64], port[8], path[64];
-    char wholeRequest[500];
-    int startPoint = 0;
-    ssize_t bytes_received;
-
-    while ((bytes_received = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
-        if (bytes_received == -1) {
-            perror("Receive failed");
-            return;
-        }
-
-        char *end_of_headers = strstr(buffer, "\r\n\r\n");
-        memcpy(wholeRequest + startPoint, buffer, bytes_received);
-        startPoint += bytes_received;
-        if (end_of_headers != NULL) {
-            printf("Printing end of headers: %s\n", end_of_headers);
-            break; // Request is complete
-        }
-    }
-
-    wholeRequest[startPoint] = '\0';
-
-    if (parse_request(wholeRequest, method, hostname, port, path)) {
-        printf("Method: %s, Hostname: %s, Port: %s, Path: %s\n", method, hostname, port, path);
-
-        char modified_request[1024];
-        sprintf(modified_request, "GET %s HTTP/1.0\r\nHost: %s:%s\r\nUser-Agent: %s\r\nConnection: close\r\nProxy-Connection: close\r\n\r\n",
-                path, hostname, port, user_agent_hdr);
-
-        int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (server_fd < 0) {
-            perror("Socket creation error");
-            return;
-        }
-
-        struct sockaddr_in server_addr;
-        memset(&server_addr, 0, sizeof(server_addr));
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(atoi(port));
-
-        struct hostent *server = gethostbyname(hostname);
-        if (server == NULL) {
-            fprintf(stderr, "Failed to resolve hostname\n");
-            close(server_fd);
-            return;
-        }
-
-        memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-        if (connect(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-            perror("Connect failed");
-            close(server_fd);
-            return;
-        }
-
-        ssize_t bytes_sent = send(server_fd, modified_request, strlen(modified_request), 0);
-        if (bytes_sent < 0) {
-            perror("Send error");
-            close(server_fd);
-            return;
-        }
-
-        char server_response[1024];
-        ssize_t server_bytes_received;
-        while ((server_bytes_received = recv(server_fd, server_response, sizeof(server_response), 0)) > 0) {
-            ssize_t response_sent = send(client_fd, server_response, server_bytes_received, 0);
-            if (response_sent < 0) {
-                perror("Response send error");
-                close(server_fd);
-                close(client_fd);
-                return;
-            }
-
-            memset(server_response, 0, sizeof(server_response));
-        }
-
-        if (server_bytes_received < 0) {
-            perror("Server receive error");
-        }
-
-        close(server_fd);
-        close(client_fd);
-        return;
-    } else {
-        printf("Failed to parse HTTP request\n");
-        close(client_fd);
-        return;
-    }
-
-    if (bytes_received < 0) {
-        perror("Client receive error");
-    }
-
-    close(client_fd);
 }
 
 int open_sfd(int port) {
